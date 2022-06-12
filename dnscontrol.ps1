664c6738cab928cff4a2a48d6ba1badf0aed2674
Usman Iqbal <Usman.Iqbal@softwire.com>
2022-06-12 19:14:42 +0100

    Add some scripts to help with dev.  Not actually working yet on their own

---
+++ dnscontrol.ps1
@@ -0,0 +1,1 @@
+docker run --rm -it -v ${pwd}/dnsconfig.js:/dns/dnsconfig.js -v ${pwd}/creds.json:/dns/creds.json stackexchange/dnscontrol dnscontrol preview
---
+++ lint.ps1
@@ -0,0 +1,1 @@
+docker run --rm -it -v ${pwd}:/tmp -w /tmp python:alpine python -m json.tool creds.js
